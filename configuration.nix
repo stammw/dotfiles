4e9211d9f35e7456251db8367d27b5e4fd31a848
Jean-Christophe BEGUE <jc.begue@pm.me>
2020-11-12 17:25:18 +0100

    Move most packages to profile

--- configuration.nix
+++ configuration.nix
@@ -14,42 +14,23 @@
 
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
-    antigen
-    binutils
     bluez
-    clang
-    coreutils
-    discord
-    emacs
-    evince
     exa
     fd
-    firefox
     fzf
     gcc
     git
     htop
-    libreoffice
     light
     nerdfonts
-    powertop
-    python37Full
-    python37Packages.ipython
-    python-language-server
     ripgrep
-    rust-analyzer
-    rustup
-    slack
     sshfs
     vim
     wget
     zsh
     unzip
-    virtualbox
-    (steam.override { withPrimus = true; extraPkgs = pkgs: [ bumblebee glxinfo ]; }).run
   ];
   environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
-  programs.steam.enable = true;
 
   fonts.fonts = with pkgs; [
     (nerdfonts.override {
@@ -115,7 +96,6 @@
       };
     };
 
-
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
@@ -131,12 +111,14 @@
     };
   };
 
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jc = {
     isNormalUser = true;
     extraGroups = [ "wheel" "audio" "video" ];
     openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCcN+hBqktUOoWG8z+u/ZjpzTTJta15hTTe6Km7oj4q4KhkpT1lUqCgVDRRe8gv+Sz+m4f5hZlmWourrFiTlc5lAyBQTaReXTNzdzJ1unkg+vPfL/GSoK5NfEC4QIszcQaPHuYdLcw09I838Xxqp/jbw5B2m9vhtGVyE51hG70q2ze88J518c6lJifNh1kxdrNO/91GnvAY7t9TmoG0Ronb5Mv8rvTn7b4zPMWzsmdorZPtzScLUsve5lcZYydruqHJ4ekJOXhFB0FChtWR/FlHpW2hJmCyHAVKKqNKAxV4BRjQevqEN+Deu+8jPf+ajHgD69XN20zs+RUMeZHwzedVCy9Tg/2wQR/c6QCSjgX1w9OxoF+0bCtavq2hra1Jh85bm/Qus+SiBB1h3J0VEXCQ+gSjOq5SVnNJTvBBM6ThCSBL3ZiA+2Bdmsij5Oik3qzjtJat1U0JFoZoDre+81a6I1Klok02Va1xMy+FAMC4pDSjsoJDCnK5y/4AxAhNeWKTxwdhsMUgr3wm5e1i5Lzb+oJLJMiaRyPhuCm7x3FxMGJ62En3rRyjYN42NDDaAqbvdbhdtzp6LNC4PUP6N6i7q8PWxLIsW9hm+x/zxxuhPa1ewumvA48B1q4YWLhzqAiUMYM4vXti6k3LMTbiBLniwK+JCxXKWvat7hyMaZYKDw== begue.jc@gmail.com" ];
   };
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- home.nix
+++ home.nix
@@ -15,7 +15,34 @@ in {
 
   home-manager.users.jc = { pkgs, ... }: {
     home.packages = with pkgs; [
+      # GUI tools
       alacritty
+      emacs
+      evince
+      discord
+
+      # CLI tools
+      fzf
+      exa
+      fd
+      ripgrep
+      unzip
+      htop
+
+      # Rust <3
+      rustup
+      rust-analyzer
+
+      # C
+      clang
+      binutils
+      coreutils
+      gcc
+
+      # Python
+      python37Full
+      python37Packages.ipython
+      python-language-server
     ];
 
     xdg.configFile."alacritty/alacritty.yml".source = configs/alacritty.yml;
@@ -29,7 +56,6 @@ in {
     programs.command-not-found.enable = true;
 
     # Editor
-
     home.file.".doom.d/init.el".source = configs/emacs/init.el;
     home.file.".doom.d/packages.el".source = configs/emacs/packages.el;
     home.file.".doom.d/config.el".source = configs/emacs/config.el;
@@ -40,5 +66,7 @@ in {
       userName = "Jean-Christpohe BEGUE";
       userEmail = "jc.begue@pm.me";
     };
+
+    programs.firefox.enable = true;
   };
 }
