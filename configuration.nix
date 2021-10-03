dc9e05dddf9fe9ec458774678fba0e811ca3d08c
Jean-Christophe BEGUE <jc.begue@pm.me>
2021-10-03 10:40:50 +0200

    Enable nix auto garbage collect

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,13 @@ in
   nixpkgs.config = {
     allowUnfree = true;
   };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   programs.steam.enable = true;
   virtualisation.docker.enable = true;
 
