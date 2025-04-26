ac81eaa9ea0802a941630ee846a39c0522b395d1
Jean-Christophe BEGUE <jc.begue+git@pm.me>
2025-04-26 18:26:14 +0200

    remove liveusb failed attempt

--- flake.nix
+++ flake.nix
@@ -3,17 +3,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    nixos-generators.url = "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
-
   };
   outputs =
     inputs@{
       self,
       nixpkgs,
       home-manager,
-      nixos-generators,
       ...
     }:
     {
@@ -43,21 +38,6 @@
             home-manager.users.jc = import ./home.nix;
             home-manager.backupFileExtension = "hm-backup";
           }
-        ];
-      };
-
-      packages.x86_64-linux.iso = inputs.nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        format = "install-iso";
-        specialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
-          {
-            system.stateVersion = "24.05";
-          }
-          ./iso.nix
         ];
       };
     };
